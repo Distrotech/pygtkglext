65850868183b1b765ccaad0df2a15dd86ef32459
Naofumi Yasufuku <naofumi@users.sourceforge.net>
2003-03-09 04:54:06 +0000

    Examples have been rewritten.

--- examples/gears-darea.py
+++ examples/gears-darea.py
@@ -8,11 +8,11 @@
 # A translation of the gears demo that comes with mesa, modified to use
 # a few GtkHScale widgets for the rotation, rather than the keyboard.
 
-from OpenGL.GL import *
 import gobject
 import gtk
 import gtk.gtkgl
 
+from OpenGL.GL import *
 import math
 
 # Draw a gear wheel.  You'll probably want to call this function when
@@ -30,13 +30,13 @@ def gear(inner_radius, outer_radius, width, teeth, tooth_depth):
 	r0 = inner_radius
 	r1 = outer_radius - tooth_depth/2.0
 	r2 = outer_radius + tooth_depth/2.0
-
+	
 	da = 2.0*math.pi / teeth / 4.0
-
+	
 	glShadeModel( GL_FLAT )
-
+	
 	glNormal3f( 0.0, 0.0, 1.0 )
-
+	
 	# draw front face 
 	glBegin( GL_QUAD_STRIP )
 	for i in range(teeth + 1):
@@ -46,7 +46,7 @@ def gear(inner_radius, outer_radius, width, teeth, tooth_depth):
 		glVertex3f( r0*cos(angle), r0*sin(angle), width*0.5 )
 		glVertex3f( r1*cos(angle+3*da), r1*sin(angle+3*da), width*0.5 )
 	glEnd();
-
+	
 	# draw front sides of teeth
 	glBegin( GL_QUADS )
 	da = 2.0*math.pi / teeth / 4.0
@@ -57,10 +57,9 @@ def gear(inner_radius, outer_radius, width, teeth, tooth_depth):
 		glVertex3f( r2*cos(angle+2*da), r2*sin(angle+2*da), width*0.5 )
 		glVertex3f( r1*cos(angle+3*da), r1*sin(angle+3*da), width*0.5 )
 	glEnd()
-
-
+	
 	glNormal3f( 0.0, 0.0, -1.0 )
-
+	
 	# draw back face
 	glBegin( GL_QUAD_STRIP )
 	for i in range(teeth + 1):
@@ -70,7 +69,7 @@ def gear(inner_radius, outer_radius, width, teeth, tooth_depth):
 		glVertex3f( r1*cos(angle+3*da), r1*sin(angle+3*da),-width*0.5 )
 		glVertex3f( r0*cos(angle), r0*sin(angle), -width*0.5 )
 	glEnd()
-
+	
 	# draw back sides of teeth
 	glBegin( GL_QUADS )
 	da = 2.0*math.pi / teeth / 4.0
@@ -82,13 +81,12 @@ def gear(inner_radius, outer_radius, width, teeth, tooth_depth):
 		glVertex3f( r2*cos(angle+da),   r2*sin(angle+da),  -width*0.5 )
 		glVertex3f( r1*cos(angle),      r1*sin(angle),     -width*0.5 )
 	glEnd()
-
-
+	
 	# draw outward faces of teeth
 	glBegin( GL_QUAD_STRIP );
 	for i in range(teeth):
 		angle = i * 2.0*math.pi / teeth
-
+		
 		glVertex3f( r1*cos(angle), r1*sin(angle),  width*0.5 )
 		glVertex3f( r1*cos(angle), r1*sin(angle), -width*0.5 )
 		u = r2*cos(angle+da) - r1*cos(angle)
@@ -108,15 +106,14 @@ def gear(inner_radius, outer_radius, width, teeth, tooth_depth):
 		glVertex3f( r1*cos(angle+3*da), r1*sin(angle+3*da), width*0.5 )
 		glVertex3f( r1*cos(angle+3*da), r1*sin(angle+3*da),-width*0.5 )
 		glNormal3f( cos(angle), sin(angle), 0.0 )
-
+	
 	glVertex3f( r1*cos(0), r1*sin(0), width*0.5 )
 	glVertex3f( r1*cos(0), r1*sin(0), -width*0.5 )
-
+	
 	glEnd()
-
-
+	
 	glShadeModel( GL_SMOOTH )
-
+	
 	# draw inside radius cylinder
 	glBegin( GL_QUAD_STRIP )
 	for i in range(teeth + 1):
@@ -136,141 +133,155 @@ gear3 = 0
 
 angle = 0.0
 
-# GLContext and GLDrawable
-glcontext = None
-gldrawable = None
-
-def xchange(adj):
-	global view_rotx
-	view_rotx = adj.value
-	draw(glarea)
-def ychange(adj):
-	global view_roty
-	view_roty = adj.value
-	draw(glarea)
-def zchange(adj):
-	global view_rotz
-	view_rotz = adj.value
-	draw(glarea)
-
-def draw(glarea, event=None):
-	global gldrawable, glcontext
+def draw(glarea, event):
+	# get GLContext and GLDrawable
+	glcontext = glarea.get_gl_context()
+	gldrawable = glarea.get_gl_drawable()
 	
-	if not gldrawable.make_current(glcontext): return
+	# GL calls
+	if not gldrawable.gl_begin(glcontext): return
 	
 	glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT )
-
+	
 	glPushMatrix()
+	
 	glRotatef( view_rotx, 1.0, 0.0, 0.0 )
 	glRotatef( view_roty, 0.0, 1.0, 0.0 )
 	glRotatef( view_rotz, 0.0, 0.0, 1.0 )
-
+	
 	glPushMatrix()
 	glTranslatef( -3.0, -2.0, 0.0 )
 	glRotatef( angle, 0.0, 0.0, 1.0 )
 	glCallList(gear1)
 	glPopMatrix()
-
+	
 	glPushMatrix()
 	glTranslatef( 3.1, -2.0, 0.0 )
 	glRotatef( -2.0*angle-9.0, 0.0, 0.0, 1.0 )
 	glCallList(gear2)
 	glPopMatrix()
-
+	
 	glPushMatrix()
 	glTranslatef( -3.1, 4.2, 0.0 )
 	glRotatef( -2.0*angle-25.0, 0.0, 0.0, 1.0 )
 	glCallList(gear3)
 	glPopMatrix()
-
+	
 	glPopMatrix()
-
+	
 	if gldrawable.is_double_buffered():
 		gldrawable.swap_buffers()
 	else:
 		glFlush()
-
-def idle():
-	global angle
-	angle = angle + 2.0
-
-	draw(glarea)
-
+	
+	gldrawable.gl_end()
+	
 	return gtk.TRUE
 
-def reshape_idle():
-	global gldrawable, glcontext
+def reshape(glarea, event):
+	# get GLContext and GLDrawable
+	glcontext = glarea.get_gl_context()
+	gldrawable = glarea.get_gl_drawable()
 	
-	if not gldrawable.make_current(glcontext): return
+	# GL calls
+	if not gldrawable.gl_begin(glcontext): return
 	
 	x, y, width, height = glarea.get_allocation()
-
+	
 	glViewport(0, 0, width, height)
 	glMatrixMode(GL_PROJECTION)
 	glLoadIdentity()
 	if width > height:
-		w = width / height
+		w = float(width) / float(height)
 		glFrustum( -w, w, -1.0, 1.0, 5.0, 60.0 )
 	else:
-		h = height / width
+		h = float(height) / float(width)
 		glFrustum( -1.0, 1.0, -h, h, 5.0, 60.0 )
-
+	
 	glMatrixMode(GL_MODELVIEW)
 	glLoadIdentity()
 	glTranslatef( 0.0, 0.0, -40.0 )
-	glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT )
-
-	return gtk.FALSE
-
-def reshape(glarea, allocation):
-	gtk.idle_add(reshape_idle)
+	
+	gldrawable.gl_end()
+	
+	return gtk.TRUE
 
 def init(glarea):
-	global gldrawable, glcontext
-	gldrawable = glarea.get_gl_drawable()
+	# get GLContext and GLDrawable
 	glcontext = glarea.get_gl_context()
+	gldrawable = glarea.get_gl_drawable()
 	
-	if not gldrawable.make_current(glcontext): return
+	# GL calls
+	if not gldrawable.gl_begin(glcontext): return
 	
 	global gear1, gear2, gear3
-
+	
 	pos = (5.0, 5.0, 10.0, 0.0 )
 	red = (0.8, 0.1, 0.0, 1.0 )
 	green = (0.0, 0.8, 0.2, 1.0 )
 	blue = (0.2, 0.2, 1.0, 1.0 )
-
+	
 	glLightfv( GL_LIGHT0, GL_POSITION, pos )
 	glEnable( GL_CULL_FACE )
 	glEnable( GL_LIGHTING )
 	glEnable( GL_LIGHT0 )
 	glEnable( GL_DEPTH_TEST )
-
+	
 	# make the gears
 	gear1 = glGenLists(1)
 	glNewList(gear1, GL_COMPILE)
 	glMaterialfv( GL_FRONT, GL_AMBIENT_AND_DIFFUSE, red )
 	gear( 1.0, 4.0, 1.0, 20, 0.7 )
 	glEndList()
-
+	
 	gear2 = glGenLists(1)
 	glNewList(gear2, GL_COMPILE)
 	glMaterialfv( GL_FRONT, GL_AMBIENT_AND_DIFFUSE, green )
 	gear( 0.5, 2.0, 2.0, 10, 0.7 )
 	glEndList()
-
+	
 	gear3 = glGenLists(1)
 	glNewList(gear3, GL_COMPILE)
 	glMaterialfv( GL_FRONT, GL_AMBIENT_AND_DIFFUSE, blue )
 	gear( 1.3, 2.0, 0.5, 10, 0.7 )
 	glEndList()
-
+	
 	glEnable( GL_NORMALIZE )
+	
+	gldrawable.gl_end()
 
+def idle(glarea):
+	global angle
+	angle = angle + 2.0
+	glarea.queue_draw()
+	return gtk.TRUE
+
+def map(glarea, event):
+	gtk.idle_add(idle, glarea)
+	return gtk.TRUE
+
+def xchange(adj):
+	global view_rotx
+	view_rotx = adj.value
+
+def ychange(adj):
+	global view_roty
+	view_roty = adj.value
+
+def zchange(adj):
+	global view_rotz
+	view_rotz = adj.value
+
+#
 # GLX version
+#
+
 major, minor = gtk.gdkgl.query_version()
 print "GLX version = %d.%d" % (major, minor)
 
-# configure frame buffer
+#
+# frame buffer configuration
+#
 
 # use GLUT-style display mode bitmask
 try:
@@ -284,56 +295,81 @@ except gtk.gdkgl.NoMatches:
 				           gtk.gdkgl.MODE_DEPTH)
 
 # use GLX-style attribute list
-#glconfig = gtk.gdkgl.Config(attrib_list = (gtk.gdkgl.RGBA,
-#					   gtk.gdkgl.DOUBLEBUFFER,
-#					   gtk.gdkgl.DEPTH_SIZE, 1))
+# try:
+# 	# try double-buffered
+# 	glconfig = gtk.gdkgl.Config(attrib_list = (gtk.gdkgl.RGBA,
+# 						   gtk.gdkgl.DOUBLEBUFFER,
+# 						   gtk.gdkgl.DEPTH_SIZE, 1))
+# except gtk.gdkgl.NoMatches:
+# 	# try single-buffered
+# 	glconfig = gtk.gdkgl.Config(attrib_list = (gtk.gdkgl.RGBA,
+# 						   gtk.gdkgl.DEPTH_SIZE, 1))
 
-print "glconfig.is_rgba() = ", glconfig.is_rgba()
-print "glconfig.is_double_buffered() = ", glconfig.is_double_buffered()
-print "glconfig.is_stereo() = ", glconfig.is_stereo()
-print "glconfig.has_alpha() = ", glconfig.has_alpha()
-print "glconfig.has_depth_buffer() = ", glconfig.has_depth_buffer()
-print "glconfig.has_stencil_buffer() = ", glconfig.has_stencil_buffer()
-print "glconfig.has_accum_buffer() = ", glconfig.has_accum_buffer()
-print "glconfig.get_n_aux_buffers() = ", glconfig.get_n_aux_buffers()
+print "glconfig.is_rgba() =",            glconfig.is_rgba()
+print "glconfig.is_double_buffered() =", glconfig.is_double_buffered()
+print "glconfig.has_depth_buffer() =",   glconfig.has_depth_buffer()
+
 # get_attrib()
-print "glconfig.get_attrib(gtk.gdkgl.DEPTH_SIZE) = %d" % glconfig.get_attrib(gtk.gdkgl.DEPTH_SIZE)
+print "gtk.gdkgl.RGBA = %d" % glconfig.get_attrib(gtk.gdkgl.RGBA)
+print "gtk.gdkgl.DOUBLEBUFFER = %d" % glconfig.get_attrib(gtk.gdkgl.DOUBLEBUFFER)
+print "gtk.gdkgl.DEPTH_SIZE = %d" % glconfig.get_attrib(gtk.gdkgl.DEPTH_SIZE)
 
-# top-level window
-win = gobject.new(gtk.Window,
-		  title='Gears')
+#
+# top-level gtk.Window
+#
+
+win = gtk.Window()
+win.set_title('Gears')
+
+win.set_resize_mode(gtk.RESIZE_IMMEDIATE)
+win.set_reallocate_redraws(gtk.TRUE)
+
 win.connect("destroy", gtk.mainquit)
+
+#
+# gtk.Table
+#
 
 table = gtk.Table(4, 2)
 win.add(table)
 table.show()
 
+#
 # gtk.gtkgl.DrawingArea for OpenGL scene
+#
+
 glarea = gtk.gtkgl.DrawingArea(glconfig)
 glarea.set_size_request(300, 300)
 
 glarea.connect("realize", init)
-glarea.connect("size_allocate", reshape)
+glarea.connect("configure_event", reshape)
 glarea.connect("expose_event", draw)
-
-gtk.idle_add(idle)
+glarea.connect("map_event", map)
 
 table.attach(glarea, 0,2, 0,1)
 glarea.show()
 
-for row, label, start, cb in ((1,'X Rotation', view_rotx, xchange),
-			      (2,'Y Rotation', view_roty, ychange),
-			      (3,'Z Rotation', view_rotz, zchange)):
+#
+# rotation sliders
+#
+
+for row, label, start, cb in ((1, 'X Rotation', view_rotx, xchange),
+			      (2, 'Y Rotation', view_roty, ychange),
+			      (3, 'Z Rotation', view_rotz, zchange)):
 	l = gtk.Label(label)
 	table.attach(l, 0,1, row,row+1, xoptions=0, yoptions=gtk.FILL)
 	l.show()
-
+	
 	adj = gtk.Adjustment(start, 0, 360, 5, 5, 0)
 	adj.connect("value_changed", cb)
-
+	
 	scale = gtk.HScale(adj)
 	table.attach(scale, 1,2, row,row+1, yoptions=gtk.FILL)
 	scale.show()
+
+#
+# main loop
+#
 
 win.show()
 
---
+++ examples/teapot.py
@@ -0,0 +1,229 @@
+#!/usr/bin/env python2.2
+#
+# This program is based on cone.py that comes with with PyGTK.
+#
+# Conversion from gtk.gl module to PyGtkGLExt by Naofumi Yasufuku
+#
+
+import gtk
+import gtk.gtkgl
+
+from OpenGL.GL import *
+from OpenGL.GLU import *
+
+rotx = 0
+roty = 0
+
+is_solid = gtk.FALSE
+
+def realize(glarea):
+	# get GLContext and GLDrawable
+	glcontext = glarea.get_gl_context()
+	gldrawable = glarea.get_gl_drawable()
+	
+	# GL calls
+	if not gldrawable.gl_begin(glcontext): return
+	
+	glMaterial(GL_FRONT, GL_AMBIENT,   [0.2, 0.2, 0.2, 1.0])
+	glMaterial(GL_FRONT, GL_DIFFUSE,   [0.8, 0.8, 0.8, 1.0])
+	glMaterial(GL_FRONT, GL_SPECULAR,  [1.0, 0.0, 1.0, 1.0])
+	glMaterial(GL_FRONT, GL_SHININESS, 50.0)
+	
+	glLight(GL_LIGHT0, GL_AMBIENT,  [0.0, 1.0, 0.0, 1.0])
+	glLight(GL_LIGHT0, GL_DIFFUSE,  [1.0, 1.0, 1.0, 1.0])
+	glLight(GL_LIGHT0, GL_SPECULAR, [1.0, 1.0, 1.0, 1.0])
+	glLight(GL_LIGHT0, GL_POSITION, [1.0, 1.0, 1.0, 0.0])
+	
+	glLightModel(GL_LIGHT_MODEL_AMBIENT, [0.2, 0.2, 0.2, 1.0])
+	
+	glEnable(GL_LIGHTING)
+	glEnable(GL_LIGHT0)
+	
+	glDepthFunc(GL_LESS)
+	glEnable(GL_DEPTH_TEST)
+		
+	gldrawable.gl_end()
+
+def configure_event(glarea, event):
+	# get GLContext and GLDrawable
+	glcontext = glarea.get_gl_context()
+	gldrawable = glarea.get_gl_drawable()
+	
+	# GL calls
+	if not gldrawable.gl_begin(glcontext): return
+	
+	x, y, width, height = glarea.get_allocation()
+	
+	glViewport(0, 0, width, height)
+	glMatrixMode(GL_PROJECTION)
+	glLoadIdentity()
+	if width > height:
+		w = float(width) / float(height)
+		glFrustum( -w, w, -1.0, 1.0, 5.0, 60.0 )
+	else:
+		h = float(height) / float(width)
+		glFrustum( -1.0, 1.0, -h, h, 5.0, 60.0 )
+	
+	glMatrixMode(GL_MODELVIEW)
+	
+	gldrawable.gl_end()
+	
+	return gtk.TRUE
+
+def expose_event(glarea, event=None):
+	# get GLContext and GLDrawable
+	glcontext = glarea.get_gl_context()
+	gldrawable = glarea.get_gl_drawable()
+	
+	# GL calls
+	if not gldrawable.gl_begin(glcontext): return
+	
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+	
+	glLoadIdentity()
+
+	glTranslate(0, 0, -10)
+	
+	glRotate(rotx, 1, 0, 0)
+	glRotate(roty, 0, 1, 0)
+	
+	gtk.gdkgl.draw_teapot(is_solid, 1)
+	
+	if gldrawable.is_double_buffered():
+		gldrawable.swap_buffers()
+	else:
+		glFlush()
+	
+	gldrawable.gl_end()
+	
+	return gtk.TRUE
+
+def vchanged(vadj, glarea):
+	global rotx
+	rotx = vadj.value
+	glarea.queue_draw()
+
+def hchanged(hadj, glarea):
+	global roty
+	roty = hadj.value
+	glarea.queue_draw()
+
+def toggled(button, glarea):
+	global is_solid
+	is_solid = ~is_solid
+	glarea.queue_draw()
+
+#
+# GLX version
+#
+
+major, minor = gtk.gdkgl.query_version()
+print "GLX version = %d.%d" % (major, minor)
+
+#
+# frame buffer configuration
+#
+
+# use GLUT-style display mode bitmask
+try:
+	# try double-buffered
+	glconfig = gtk.gdkgl.Config(mode = gtk.gdkgl.MODE_RGB    |
+				           gtk.gdkgl.MODE_DOUBLE |
+				           gtk.gdkgl.MODE_DEPTH)
+except gtk.gdkgl.NoMatches:
+	# try single-buffered
+	glconfig = gtk.gdkgl.Config(mode = gtk.gdkgl.MODE_RGB    |
+				           gtk.gdkgl.MODE_DEPTH)
+
+# use GLX-style attribute list
+# try:
+# 	# try double-buffered
+# 	glconfig = gtk.gdkgl.Config(attrib_list = (gtk.gdkgl.RGBA,
+# 						   gtk.gdkgl.DOUBLEBUFFER,
+# 						   gtk.gdkgl.DEPTH_SIZE, 1))
+# except gtk.gdkgl.NoMatches:
+# 	# try single-buffered
+# 	glconfig = gtk.gdkgl.Config(attrib_list = (gtk.gdkgl.RGBA,
+# 						   gtk.gdkgl.DEPTH_SIZE, 1))
+
+print "glconfig.is_rgba() =",            glconfig.is_rgba()
+print "glconfig.is_double_buffered() =", glconfig.is_double_buffered()
+print "glconfig.has_depth_buffer() =",   glconfig.has_depth_buffer()
+
+# get_attrib()
+print "gtk.gdkgl.RGBA = %d" % glconfig.get_attrib(gtk.gdkgl.RGBA)
+print "gtk.gdkgl.DOUBLEBUFFER = %d" % glconfig.get_attrib(gtk.gdkgl.DOUBLEBUFFER)
+print "gtk.gdkgl.DEPTH_SIZE = %d" % glconfig.get_attrib(gtk.gdkgl.DEPTH_SIZE)
+
+#
+# top-level gtk.Window
+#
+
+win = gtk.Window()
+win.set_title("Teapot")
+
+win.set_resize_mode(gtk.RESIZE_IMMEDIATE)
+win.set_reallocate_redraws(gtk.TRUE)
+
+win.connect("destroy", gtk.mainquit)
+
+#
+# gtk.Table
+#
+
+table = gtk.Table(2, 4)
+table.set_border_width(5)
+table.set_col_spacings(5)
+table.set_row_spacings(5)
+win.add(table)
+table.show()
+
+#
+# gtk.DrawingArea for OpenGL scene
+#
+
+glarea = gtk.gtkgl.DrawingArea(glconfig)
+glarea.set_size_request(300, 300)
+
+glarea.connect("realize", realize)
+glarea.connect("configure_event", configure_event)
+glarea.connect("expose_event", expose_event)
+
+table.attach(glarea, 0,1, 0,1)
+glarea.show()
+
+#
+# rotation sliders
+#
+
+vadj = gtk.Adjustment(0, -360, 360, 5, 5, 0)
+vadj.connect("value_changed", vchanged, glarea)
+
+vscale = gtk.VScale(vadj)
+table.attach(vscale, 1,2, 0,1, xoptions=gtk.FILL)
+vscale.show()
+
+hadj = gtk.Adjustment(0, -360, 360, 5, 5, 0)
+hadj.connect("value_changed", hchanged, glarea)
+
+hscale = gtk.HScale(hadj)
+table.attach(hscale, 0,1, 1,2, yoptions=gtk.FILL)
+hscale.show()
+
+#
+# toggle button
+#
+
+button = gtk.ToggleButton('solid')
+button.connect("toggled", toggled, glarea)
+table.attach(button, 0,2, 2,3, gtk.FILL, gtk.FILL)
+button.show()
+
+#
+# main loop
+#
+
+win.show()
+
+gtk.mainloop()
+
