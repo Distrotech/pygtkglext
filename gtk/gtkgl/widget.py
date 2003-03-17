7883522d3f226cb91bf0ff10535d5d0a4add8896
Naofumi Yasufuku <naofumi@users.sourceforge.net>
2003-03-17 10:33:13 +0000

    Subclassing built-in object.

--- gtk/gtkgl/widget.py
+++ gtk/gtkgl/widget.py
@@ -18,32 +18,46 @@
 import gtk
 from _gtkgl import *
 
-class Widget:
+### base class for creating an OpenGL-capable widget
+
+class Widget(object):
+    """Base class for creating an OpenGL-capable widget.
+    """
+    
+    def __init__(self):
+        raise NotImplementedError, \
+              "%s is an abstract base class" % (self.__class__)
+    
     def set_gl_capability(self, glconfig, share_list=None, direct=gtk.TRUE,
                           render_type=gtk.gdkgl.RGBA_TYPE):
         return widget_set_gl_capability(self, glconfig, share_list,
                                         direct, render_type)
-
+    
     def is_gl_capable(self):
         return widget_is_gl_capable(self)
-
+    
     def get_gl_config(self):
         return widget_get_gl_config(self)
-
+    
     def create_gl_context(self, share_list=None, direct=gtk.TRUE,
                           render_type=gtk.gdkgl.RGBA_TYPE):
         return widget_create_gl_context(self, share_list, direct, render_type)
-
+    
     def get_gl_context(self):
         return widget_get_gl_context(self)
-
+    
     def get_gl_window(self):
         return widget_get_gl_window(self)
-
+    
     def get_gl_drawable(self):
         return widget_get_gl_drawable(self)
 
+### OpenGL-capable gtk.DrawingArea
+
 class DrawingArea(gtk.DrawingArea, Widget):
+    """OpenGL-capable gtk.DrawingArea.
+    """
+    
     def __init__(self, glconfig=None, share_list=None, direct=gtk.TRUE,
                  render_type=gtk.gdkgl.RGBA_TYPE):
         gtk.DrawingArea.__init__(self)
